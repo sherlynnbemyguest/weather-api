afb71f0131ce87198bceb4dc0f97170f33ea97f6
Sherlynn Parreno <sherlynngailparreno@gmail.com>
2020-04-25 19:01:27 +0800

    Updates

---
+++ precache-manifest.6c465145dd85612d1b389c716ee7b06b.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "892962575bff0be097b0db0a18ccafe0",
+    "url": "/weather-api/index.html"
+  },
+  {
+    "revision": "a9dc35dd78fab80570a5",
+    "url": "/weather-api/static/css/main.f837233d.chunk.css"
+  },
+  {
+    "revision": "ef7c920c0e828c4c8815",
+    "url": "/weather-api/static/js/2.2f82013c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weather-api/static/js/2.2f82013c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9dc35dd78fab80570a5",
+    "url": "/weather-api/static/js/main.0d47319e.chunk.js"
+  },
+  {
+    "revision": "84925c6ec3bf2703b571",
+    "url": "/weather-api/static/js/runtime-main.574f2d64.js"
+  },
+  {
+    "revision": "7f93fbdb8a9d02fe414c0a0119b77bd4",
+    "url": "/weather-api/static/media/01d.7f93fbdb.svg"
+  },
+  {
+    "revision": "a3f3f38d2c17971938ebd891fb6bbd0c",
+    "url": "/weather-api/static/media/01n.a3f3f38d.svg"
+  },
+  {
+    "revision": "80a5d865c63ba642a927daba54438305",
+    "url": "/weather-api/static/media/02d.80a5d865.svg"
+  },
+  {
+    "revision": "7e65840bf2249d8f7ba19fc029438545",
+    "url": "/weather-api/static/media/02n.7e65840b.svg"
+  },
+  {
+    "revision": "ad789581dd3bfe802dd2e6ea0c2f5caa",
+    "url": "/weather-api/static/media/03d.ad789581.svg"
+  },
+  {
+    "revision": "ad789581dd3bfe802dd2e6ea0c2f5caa",
+    "url": "/weather-api/static/media/03n.ad789581.svg"
+  },
+  {
+    "revision": "ad789581dd3bfe802dd2e6ea0c2f5caa",
+    "url": "/weather-api/static/media/04d.ad789581.svg"
+  },
+  {
+    "revision": "ad789581dd3bfe802dd2e6ea0c2f5caa",
+    "url": "/weather-api/static/media/04n.ad789581.svg"
+  },
+  {
+    "revision": "2bd236411dc612e871d9c314239c3fa7",
+    "url": "/weather-api/static/media/09d.2bd23641.svg"
+  },
+  {
+    "revision": "d235ab65ea35c3fcade47b8f5c6c4ae7",
+    "url": "/weather-api/static/media/09n.d235ab65.svg"
+  },
+  {
+    "revision": "f134a51c3c0a49f33fecc7a201a4e075",
+    "url": "/weather-api/static/media/10d.f134a51c.svg"
+  },
+  {
+    "revision": "c915395f8c3e3571d734680505ceeb11",
+    "url": "/weather-api/static/media/10n.c915395f.svg"
+  },
+  {
+    "revision": "7142c78bba5ddb0b77c3ccddcebe96ab",
+    "url": "/weather-api/static/media/11d.7142c78b.svg"
+  },
+  {
+    "revision": "7142c78bba5ddb0b77c3ccddcebe96ab",
+    "url": "/weather-api/static/media/11n.7142c78b.svg"
+  },
+  {
+    "revision": "d103c77cf7f40bc52812fe945ddd3cfa",
+    "url": "/weather-api/static/media/13d.d103c77c.svg"
+  },
+  {
+    "revision": "3def7c9fb95731ac622871fb6925d5f6",
+    "url": "/weather-api/static/media/13n.3def7c9f.svg"
+  },
+  {
+    "revision": "c3be8cbba61a6e6d883ba7d3b404f519",
+    "url": "/weather-api/static/media/HelveticaNeue-Bold.c3be8cbb.ttf"
+  },
+  {
+    "revision": "abaac753f68bee4eb719bbf49cde0136",
+    "url": "/weather-api/static/media/HelveticaNeue-Regular.abaac753.ttf"
+  },
+  {
+    "revision": "2738019da90b9fac396ae4397e017adb",
+    "url": "/weather-api/static/media/weather-sprite.2738019d.svg"
+  },
+  {
+    "revision": "56c46d80c13ec13e8d5062a8d53620b7",
+    "url": "/weather-api/static/media/weather.56c46d80.svg"
+  },
+  {
+    "revision": "166882bcaca1e09fb3d1d83d8b847eb5",
+    "url": "/weather-api/static/media/weather_sagittarius.166882bc.svg"
+  },
+  {
+    "revision": "cf01f54e3e7bdcf0fabc86a5719d648b",
+    "url": "/weather-api/static/media/weather_sunset.cf01f54e.svg"
+  }
+]);
